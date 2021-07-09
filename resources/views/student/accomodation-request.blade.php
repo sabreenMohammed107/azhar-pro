3c58357e9f85c3aa1a5ef27f13be14695cd13627
sabreenMohammed107 <sabreenm312@gmail.com>
2021-07-09 16:53:33 +0200

    test

--- resources/views/student/accomodation-request.blade.php
+++ resources/views/student/accomodation-request.blade.php
@@ -57,7 +57,7 @@
 					<label for="gender">Gender</label>
 					<select name="gender" class="form-control form-control-lg" id="gender">
 						<option value="1" @if($row->gender=="1") selected @endif>Male</option>
-						<option value="0" @if($row->gender !="1")>selected @endif>Female</option>
+						<option value="0" @if($row->gender !="1") selected @endif>Female</option>
 					</select>
 				</div>
 				<div class="col-md-6 form-group">
